de021c0208e9cb74778faa2518bbcca0e0fef03d
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-10 14:57:43 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers,string) {
+    return drivers.filter((drivers)=>{
+        if (drivers.toLowerCase()===string.toLowerCase()) {
+            return string
+        }
+    })
+}
+function fuzzyMatch(drivers,string) {
+    return drivers.filter((drivers)=>{
+        if (drivers.charAt(0)===string.charAt(0)) {
+            return string
+        }
+    })
+}
+function matchName(drivers,string) {
+    return drivers.filter((drivers)=>{
+        if (drivers.name===string) {
+            return string
+        }
+    })
+}
